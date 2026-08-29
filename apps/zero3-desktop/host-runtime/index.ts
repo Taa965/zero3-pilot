25ff9a0ddcf64017c62411de68832797330329ca
Taa965 <aaaa@246i.com>
2026-08-30 04:26:16 +0800

    feat(desktop): export remote host runtime

---
+++ apps/zero3-desktop/host-runtime/index.ts
@@ -0,0 +1,8 @@
+export { Zero3RemoteNode } from './remote-node'
+export type { Zero3CodexRuntime } from './remote-task-runner'
+export type {
+  Zero3RemoteHostConfig,
+  Zero3RemoteHostStatus,
+  Zero3RemoteLease,
+  Zero3RemoteTask
+} from './remote-types'
