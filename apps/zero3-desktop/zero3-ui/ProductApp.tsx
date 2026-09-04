88dcdc71c3e5bcf1f37777e4f2f95fca103e5337
Taa965 <aaaa@246i.com>
2026-09-04 22:19:41 +0800

    feat(ui): compose handoff controls into the owned renderer

---
+++ apps/zero3-desktop/zero3-ui/ProductApp.tsx
@@ -0,0 +1,11 @@
+import { App } from './App'
+import { HandoffDock } from './HandoffDock'
+
+export function ProductApp() {
+  return (
+    <>
+      <App />
+      <HandoffDock />
+    </>
+  )
+}
