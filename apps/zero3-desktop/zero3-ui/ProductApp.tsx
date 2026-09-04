d49042a1e3ab364fa56e47329d612c802bb8cc90
Taa965 <aaaa@246i.com>
2026-09-04 23:12:16 +0800

    feat(ui): compose project and task docks

--- apps/zero3-desktop/zero3-ui/ProductApp.tsx
+++ apps/zero3-desktop/zero3-ui/ProductApp.tsx
@@ -1,11 +1,15 @@
 import { App } from './App'
 import { HandoffDock } from './HandoffDock'
+import { ProjectDock } from './ProjectDock'
+import { TaskDock } from './TaskDock'
 
 export function ProductApp() {
   return (
     <>
       <App />
       <HandoffDock />
+      <ProjectDock />
+      <TaskDock />
     </>
   )
 }
